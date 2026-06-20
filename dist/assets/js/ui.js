416dd4931db6bd73852eec87980766c0041d727c
kik8727 <kik87827@gmail.com>
2026-06-20 19:25:12 +0900

    pc 전체메뉴 완료료

--- dist/assets/js/ui.js
+++ dist/assets/js/ui.js
@@ -211,3 +211,167 @@ function rankLayerFunc() {
   }
   rankFunc();
 }
+
+
+
+/* popup */
+class DesignPopup {
+  constructor(option) {
+    // variable
+    this.option = option;
+    this.selector = document.querySelector(this.option.selector);
+    this.touchstart = "ontouchstart" in window;
+    if (!this.selector) {
+      return;
+    }
+
+    this.design_popup_wrap = document.querySelectorAll(".popup_wrap");
+    this.domHtml = document.querySelector("html");
+    this.domBody = document.querySelector("body");
+    this.pagewrap = document.querySelector(".page_wrap");
+    this.layer_wrap_parent = null;
+    this.btn_closeTrigger = null;
+    this.scrollValue = 0;
+
+    // init
+    const popupGroupCreate = document.createElement("div");
+    popupGroupCreate.classList.add("layer_wrap_parent");
+    if (!this.layer_wrap_parent && !document.querySelector(".layer_wrap_parent")) {
+      this.pagewrap.append(popupGroupCreate);
+    }
+    this.layer_wrap_parent = document.querySelector(".layer_wrap_parent");
+
+    // event
+    this.btn_close = this.selector.querySelectorAll(".btn_popup_close");
+    this.bg_design_popup = this.selector.querySelector(".bg_dim");
+    let closeItemArray = [...this.btn_close];
+    if (!!this.selector.querySelectorAll(".close_trigger")) {
+      this.btn_closeTrigger = this.selector.querySelectorAll(".close_trigger");
+      closeItemArray.push(...this.btn_closeTrigger);
+    }
+    if (closeItemArray.length) {
+      closeItemArray.forEach((element) => {
+        element.addEventListener(
+          "click",
+          (e) => {
+            e.preventDefault();
+            this.popupHide(this.selector);
+          },
+          false
+        );
+      });
+    }
+  }
+  dimCheck() {
+    const popupActive = document.querySelectorAll(".popup_wrap.active");
+    if (!!popupActive[0]) {
+      popupActive[0].classList.add("active_first");
+    }
+    if (popupActive.length > 1) {
+      this.layer_wrap_parent.classList.add("has_active_multi");
+    } else {
+      this.layer_wrap_parent.classList.remove("has_active_multi");
+    }
+  }
+  popupShow(option) {
+    let target = this.option.selector;
+    let instance_option = option || {};
+    this.design_popup_wrap_active = document.querySelectorAll(".popup_wrap.active");
+    if (this.selector == null) {
+      return;
+    }
+    if (this.touchstart) {
+      this.domHtml.classList.add("touchDis");
+    }
+    this.selector.classList.add("active");
+    setTimeout(() => {
+      this.selector.classList.add("motion_end");
+      if ("openCallback" in instance_option) {
+        instance_option.openCallback();
+      }
+    }, 30);
+    if ("beforeCallback" in this.option) {
+      this.option.beforeCallback();
+    }
+    if ("callback" in this.option) {
+      this.option.callback();
+    }
+    this.layer_wrap_parent.append(this.selector);
+    this.dimCheck();
+  }
+  popupHide(option) {
+    let target = this.option.selector;
+    let instance_option = option || {};
+    if (!!target) {
+      this.selector.classList.remove("motion");
+      if ("beforeClose" in this.option) {
+        this.option.beforeClose();
+      }
+      if ("beforeClose" in instance_option) {
+        instance_option.beforeClose();
+      }
+      //remove
+      this.selector.classList.remove("motion_end");
+      setTimeout(() => {
+        this.selector.classList.remove("active");
+        let closeTimer = 0;
+        if (closeTimer) {
+          clearTimeout(closeTimer);
+          closeTimer = 0;
+        } else {
+          if ("closeCallback" in this.option) {
+            this.option.closeCallback();
+          }
+          closeTimer = setTimeout(() => {
+            if ("closeCallback" in instance_option) {
+              instance_option.closeCallback();
+            }
+          }, 30);
+        }
+      }, 400);
+      this.design_popup_wrap_active = document.querySelectorAll(".popup_wrap.active");
+      this.dimCheck();
+
+      if (this.design_popup_wrap_active.length == 1) {
+        this.domHtml.classList.remove("touchDis");
+      }
+    }
+  }
+}
+
+
+function pcTotalFunc() {
+  const desk_tabmenu = $(".desk_tabmenu.d_tab");
+  const desk_depth_wrap = $(".desk_depth_wrap");
+  const desk_total_layer = $(".desk_total_layer");
+  const desk_panel_container = $(".desk_panel_container");
+  const hgroup_side_totalmenu = $(".hgroup_side_totalmenu");
+  const btn_desk_total_close = $(".btn_desk_total_close");
+  desk_tabmenu.on("click", function(e) {
+    e.preventDefault();
+    const $this = $(this);
+    const $target = $($this.attr("href"));
+    desk_tabmenu.removeClass("active");
+    $this.addClass("active");
+    if ($target) {
+      desk_depth_wrap.removeClass("active");
+      $target.addClass("active");
+    }
+  });
+  btn_desk_total_close.on("click", function(e) {
+    e.preventDefault();
+    hgroup_side_totalmenu.removeClass("active");
+    desk_total_layer.removeClass("active");
+  });
+  hgroup_side_totalmenu.on("click", function(e) {
+    e.preventDefault();
+    $(this).toggleClass("active");
+    desk_total_layer.toggleClass("active");
+  });
+  $(document).on("click", function(e) {
+    if ($(e.target).closest(".desk_panel_container,.hgroup_side_totalmenu").length === 0) {
+      hgroup_side_totalmenu.removeClass("active");
+      desk_total_layer.removeClass("active");
+    }
+  });
+}
