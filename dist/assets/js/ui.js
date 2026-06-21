5c922e9d7da4ca25f87912fb20199b0aa3036d61
kik8727 <kik87827@gmail.com>
2026-06-21 21:49:21 +0900

    레이어 작업 완료료

--- dist/assets/js/ui.js
+++ dist/assets/js/ui.js
@@ -1,10 +1,15 @@
 window.addEventListener("DOMContentLoaded", () => {
   uiBase.init();
+  commonResize();
 });
 window.addEventListener("load", () => {
   headerMenu();
   rankLayerFunc();
+  posLayerEvent();
 });
+
+
+
 
 const uiBase = {
   init() {
@@ -478,6 +483,24 @@ function pcTotalFunc() {
 }
 
 
+function whereContainerFunc() {
+  const sort_cateitem = $(".sort_cateitem");
+  const sort_catecont_wrap = $(".sort_catecont_wrap");
+  const sort_chkitem_container = $(".sort_chkitem_container");
+  sort_cateitem.on("click", function(e) {
+    e.preventDefault();
+    const $this = $(this);
+    const $target = $($this.attr("href"));
+    sort_cateitem.removeClass("active");
+    $this.addClass("active");
+    if ($target) {
+      sort_catecont_wrap.removeClass("active");
+      $target.addClass("active");
+    }
+  });
+}
+
+
 function mainBannerSwiperFunc() {
   const mainBannerSwiper = new Swiper(".swiper-container.mv-swiper", {
     slidesPerView: "auto",
@@ -509,4 +532,163 @@ function mainBannerSwiperFunc() {
       },
     },
   });
+}
+
+function commonResize() {
+  var $window_width = 0;
+  $(window)
+    .on("resize", function() {
+      if ($window_width == $(window).width()) {
+        return;
+      }
+      posLayerResize();
+    })
+    .resize();
+}
+
+function posLayerEvent() {
+  var posCallBtn = $("[data-poslayer]");
+  var poslayer_z = $(".poslayer_z");
+
+  $("body").append(poslayer_z);
+
+  posCallBtn.on("click", function(e) {
+    var $this = $(this),
+      $t_t = $($this.attr("data-poslayer"));
+    e.preventDefault();
+    posLayerShow($t_t, $this);
+  });
+  poslayer_z.on("click", ".layerclose", function(e) {
+    e.preventDefault();
+    posLayerHide($(this).parents(".poslayer_z"));
+  });
+
+  $(document).on("click", ".btn_psubmit", function(e) {
+    e.preventDefault();
+    let thisParent = $(this).parents(".poslayer_z");
+    let thisSubParent = thisParent.find(".sort_chkitem_container");
+    let targetCols = $(`[data-poslayer='#${thisParent.attr("id")}']`);
+    let activeDate = thisParent.attr("data-date");
+    let activeText = thisParent.find(".pclayer_vlist .pcv_chk.active").text();
+    let activeSccOneText = thisParent.find(".sort_cateitem_title.active").text();
+    let activeSccTwoText = thisParent.find(".scc_item.active").text();
+
+    if (thisSubParent.length) {
+      targetCols.find(".ms_node_result").html((activeSccOneText + "/" + activeSccTwoText).replace(/^\/+|\/+$/g, ""));
+    } else {
+      targetCols.find(".ms_node_result").html(activeText);
+    }
+    if (targetCols.find(".ms_node_result").text().length) {
+      targetCols.addClass("result_mode");
+    }
+    posLayerHide(thisParent);
+  });
+
+  $(document).on("click", ".pcv_chk , .scc_item, .sort_cateitem_title", function(e) {
+    e.preventDefault();
+    $(this).parents("li").siblings().find(".pcv_chk,.scc_item, .sort_cateitem_title").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  $(document).on("click", function(e) {
+    if (!$(e.target).parents("[data-poslayer] , .poslayer_z , .layer_in_control").length && !$(e.target).is("[data-poslayer]") && !$(e.target).is(".layer_in_control")) {
+      posLayerHide($(".poslayer_z.active"));
+    }
+  });
+}
+
+function posLayerShow(target, btn) {
+  var poslayer_z = $(".poslayer_z");
+  var target = $(target);
+
+  $("body").append(target);
+  if (target.hasClass("mb_full") && $(window).width() < 1024 && "ontouchstart" in window) {
+    $("html,body").addClass("touchDis");
+  }
+  poslayer_z.removeClass("active");
+  target.addClass("active");
+  posLayerPos(target, btn);
+}
+
+function posLayerResize() {
+  var poslayer_z = $(".poslayer_z");
+  if (poslayer_z.length) {
+    poslayer_z.each(function() {
+      posLayerResizeAction($(this));
+    });
+  }
+}
+
+function posLayerPos(target, btn) {
+  var $target = $(target);
+  var $target_tvitdep = $target.find(".tvitdep_vlist_wrap");
+  var $target_tvitdep_pos = $target_tvitdep.length ? $target_tvitdep.offset().left : 0;
+  var $target_tvitdep_wid = $target_tvitdep.length ? $target_tvitdep.outerWidth() : 0;
+  var $targetWid = $target.length ? $target.outerWidth() : 0;
+  var $btn = $(btn);
+  var $btnIndex = $btn.index();
+  var $btnPosTop = $btn.length ? $btn.offset().top : 0;
+  var $btnPosHeight = $btn.length ? $btn.outerHeight() : 0;
+  var $btnPosLeft = $btn.length ? $btn.offset().left : 0;
+  var $btnWid = $btn.length ? $btn.outerWidth() : 0;
+
+  var $margin = $(window).width() < 1024 ? 0 : 10;
+  var elseMargin = 0;
+  $target.css({
+    top: "",
+    left: "",
+    right: "",
+    width: ""
+  });
+  if ($targetWid + $btnPosLeft > $(window).width()) {
+    $target.css({
+      top: $btnPosTop + $btnPosHeight + $margin,
+      left: "auto",
+      right: 20,
+    });
+  } else {
+    $target.css({
+      top: $btnPosTop + $btnPosHeight + $margin,
+      left: $btnPosLeft,
+    });
+  }
+}
+
+function posLayerResizeAction(target) {
+  var $target = $(target);
+  var $target_tvitdep = $target.find(".tvitdep_vlist_wrap");
+  var $target_tvitdep_pos = $target_tvitdep.length ? $target_tvitdep.offset().left : 0;
+  var $target_tvitdep_wid = $target_tvitdep.length ? $target_tvitdep.outerWidth() : 0;
+  var $targetWid = $target.length ? $target.outerWidth() : 0;
+  var $btn = $("[data-poslayer='#" + $target.attr("id") + "']");
+  var $btnIndex = $btn.index();
+  var $btnPosTop = $btn.length ? $btn.offset().top : 0;
+  var $btnPosHeight = $btn.length ? $btn.outerHeight() : 0;
+  var $btnPosLeft = $btn.length ? $btn.offset().left : 0;
+  var $btnWid = $btn.length ? $btn.outerWidth() : 0;
+  $target.css({
+    top: "",
+    left: "",
+    right: "",
+    width: ""
+  });
+  var $margin = $(window).width() < 1024 ? 0 : 10;
+  if ($targetWid + $btnPosLeft > $(window).width()) {
+    $target.css({
+      top: $btnPosTop + $btnPosHeight + $margin,
+      left: "auto",
+      right: 20,
+    });
+  } else {
+    $target.css({
+      top: $btnPosTop + $btnPosHeight + $margin,
+      left: $btnPosLeft,
+    });
+  }
+}
+
+function posLayerHide(target) {
+  var target = $(target) || target;
+  target.removeClass("active");
+  $("html,body").removeClass("touchDis");
 }
